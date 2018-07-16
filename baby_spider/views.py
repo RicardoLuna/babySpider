46fc5cefe380e81150ef408cb3d815d82cbaf98e
Unknown <cardo.luna@gmail.com>
2018-07-16 17:02:36 -0300

    [DjangoProject] Adiciona filtro par query

--- baby_spider/views.py
+++ baby_spider/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Spider, Extract
 
 # Create your views here.
 def spider_list(request):
+    return render(request, 'baby_spider/spider_list.html', {})
+
+
+def extract_list(request, name):
+    posts = Post.objects.filter(author=name)
     return render(request, 'baby_spider/spider_list.html', {})
